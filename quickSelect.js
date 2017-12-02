707762de8db619e4fe3ce10d22d768d9c642aadc
raushankumarnitdgp <raushankumar.nitdgp@gmail.com>
2017-12-02 20:55:51 +0530

    Added randomised QuickSort

--- quickSelect.js
+++ quickSelect.js
@@ -1,1 +1,30 @@
 var arr = [12, 3, 5, 7, 4, 19, 26];
+var k = 3;
+
+var partition = function (arr, l, r) {
+    var j = l - 1;
+    for (var i = l; i <= r; i++) {
+        if(arr[i] <= arr[r]) {
+            j++;
+            var t = arr[j];
+            arr[j] = arr[i];
+            arr[i] = t; 
+        }
+    }
+    return j;
+}
+
+var quickSelect = function (arr, l, r, k) {
+    if (l <= r) {
+        var pivIndex = partition(arr, l, r);
+        if (pivIndex - l + 1 === k) {
+            return arr[pivIndex];
+        }
+        if (pivIndex - l + 1 > k) {
+            return quickSelect(arr, l, pivIndex - 1, k);
+        }
+        return quickSelect(arr, pivIndex + 1, r, k - pivIndex + l - 1);
+    }
+    return 99999;
+}
+console.log(quickSelect(arr, 0, arr.length - 1, k));
